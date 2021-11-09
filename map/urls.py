d1a1de86325ab65dd604329f9918b2fb61748480
kazuki0259 <ksw2070302@stu.o-hara.ac.jp>
2021-11-09 15:43:43 +0900

    現在地表示

--- map/urls.py
+++ map/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('mapTest/', views.MapTestView.as_view(), name='mapTest'),
 ]
--- map/views.py
+++ map/views.py
@@ -4,3 +4,5 @@ from django.views import generic
 # Create your views here.
 class IndexView(generic.TemplateView):
     template_name="index.html"
+class MapTestView(generic.TemplateView):
+    template_name="mapTest.html"
