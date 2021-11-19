e3ba32b3ca4a371cb3335a5a5b1f947534e1f980
sakata yuuki <yuuki.syou.rui@gmail.com>
2021-11-19 16:40:18 +0900

    クリックイベントの追加

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,8 @@
 function gmap(){
   var sc = document.createElement('script');
-  sc.src = 'https://maps.googleapis.com/maps/api/js?key='+config.GOOGLEMAP_API_KEY+'&libraries=places&callback=initMap&v=weekly&v=beta';
+  sc.src = 'https://maps.googleapis.com/maps/api/js?key='+conf.MAP_KEY+'&libraries=places&callback=initMap&v=weekly&v=beta';
   document.body.appendChild(sc);
 }
-
 // [START maps_places_searchbox]
 // This example adds a search box to a map, using the Google Place Autocomplete
 // feature. People can enter geographical searches. The search box will return a
@@ -11,18 +10,14 @@ function gmap(){
 // This example requires the Places library. Include the libraries=places
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 35.859766, lng: 139.971014},
     zoom: 15,
     heading: 0,
     tilt: 0,
-    mapId: "90f87356969d889c",
+    // mapId: "90f87356969d889c",
   });
-
-  // const GOOGLEMAP_API_KEY = config.apikey;
-  // console.log(GOOGLEMAP_API_KEY); // ← ブラウザで確認できる。
 
   //情報ウィンドウのインスタンスの生成（後でマーカーに紐付け）
   var infowindow = new google.maps.InfoWindow();
@@ -47,9 +42,9 @@ function initMap() {
       lng: position.coords.longitude
     };
     //情報ウィンドウに現在位置を指定
-    infowindow.setPosition(pos);
+    // infowindow.setPosition(pos);
     //情報ウィンドウのコンテンツを設定
-    infowindow.setContent('現在位置を取得しました。');
+    // infowindow.setContent('現在位置を取得しました。');
     //情報ウィンドウを表示
     // infowindow.open(map);
     //マップの中心位置を指定
@@ -63,11 +58,10 @@ function initMap() {
       //キーワードで検索する場合は name:'レストラン' や ['レストラン','中華'] のように指定
     }, callback);  //コールバック関数（callback）は別途定義
 
+    // console.log(pos['lat']);
     //コールバック関数には results, status が渡されるので、status により条件分岐
     function callback(results, status) {
-      const HOTPEPPER_API_KEY = config.hotkey;
-      // console.log(HOTPEPPER_API_KEY); // ← ブラウザで確認できる。
-      var urlw = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key='+HOTPEPPER_API_KEY+'&lat='+pos["lat"]+'&lng='+pos["lng"]+'&range=3&order=4&count=50&format=jsonp';
+      var urlw ='http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key='+conf.GOURMET_KEY+'&lat='+pos["lat"]+'&lng='+pos["lng"]+'&range=3&order=4&count=50&format=jsonp';
       $.ajax({
         url: urlw,
         type: 'GET',
@@ -84,13 +78,13 @@ function initMap() {
         console.log("no"); // 失敗時
       });
       // status は以下のような定数で判定（OK の場合は results が配列で返ってきます）
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        //results の数だけ for 文で繰り返し処理
-        for (var i = 0; i < results.length; i++) {
-          //createMarker() はマーカーを生成する関数（別途定義）
-          createMarker(results[i]);
-        }
-      }
+      // if (status === google.maps.places.PlacesServiceStatus.OK) {
+      //   //results の数だけ for 文で繰り返し処理
+      //   for (var i = 0; i < results.length; i++) {
+      //     //createMarker() はマーカーを生成する関数（別途定義）
+      //     createMarker(results[i]);
+      //   }
+      // }
     }
   }, function() {  //位置情報の取得をユーザーがブロックした場合のコールバック
     //情報ウィンドウの位置をマップの中心位置に指定
@@ -101,25 +95,35 @@ function initMap() {
     infowindow.open(map);
   });   
   
-  //マーカーを生成する関数（引数には検索結果の配列 results[i] が入ってきます）
-  function createMarker(place) {
-    //var placeLoc = place.geometry.location; 
-    var marker = new google.maps.Marker({
-      map: map,
-      position: place.geometry.location  //results[i].geometry.location
-    });
- 
-    //マーカーにイベントリスナを設定
-    marker.addListener('click', function() {
-      var i = place.name + "<br>" + place.rating;
-      infowindow.setContent(i);  //results[i].name
-      infowindow.open(map, this);
-    });
-  }
+  // マーカーを生成する関数（引数には検索結果の配列 results[i] が入ってきます）
+  // function createMarker(place) {
+  //   deleteMarker();
 
-  
+  //   //var placeLoc = place.geometry.location; 
+  //   var marker = new google.maps.Marker({
+  //     map: map,
+  //     position: place.geometry.location  //results[i].geometry.location
+  //   });
+  //   // console.log(place);
+  //   //マーカーにイベントリスナを設定
+  //   marker.addListener('click', function() {
+  //     var i = place.name + "<br>★：" + place.rating + "<br>" + place.formatted_address + "<br>";
+  //     infowindow.setContent(i);  //results[i].name
+  //     infowindow.open(map, this);
+  //   });
+  //   //マーカーを削除する
+  //   function deleteMarker() {
+  //     if(marker != null){
+  //       marker.setMap(null);
+  //     }
+  //     marker = null;
+  //   }
+  // }
+
+  let list = [];
 
   function gourmetMarker(res) {
+    deleteMakers();
     //var placeLoc = place.geometry.location; 
     var marker = new google.maps.Marker({
       map: map,
@@ -132,6 +136,14 @@ function initMap() {
       infowindow.setContent(i);  //results[i].name
       infowindow.open(map, this);
     });
+
+    function deleteMakers() {
+      if(marker != null){
+        marker.setMap(null);
+      }
+      marker = null;
+    }
+    list.push(marker);
   }
 
   const buttons = [
@@ -167,6 +179,69 @@ function initMap() {
     }
   };
   
+//   // Create the search box and link it to the UI element.
+//   const input = document.getElementById("pac-input");
+//   const searchBox = new google.maps.places.SearchBox(input);
+
+//   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+//   // Bias the SearchBox results towards current map's viewport.
+//   map.addListener("bounds_changed", () => {
+//     searchBox.setBounds(map.getBounds());
+//   });
+
+//   let markers = [];
+
+//   // Listen for the event fired when the user selects a prediction and retrieve
+//   // more details for that place.
+//   searchBox.addListener("places_changed", () => {
+//     const places = searchBox.getPlaces();
+
+//     if (places.length == 0) {
+//       return;
+//     }
+
+//     // Clear out the old markers.
+//     markers.forEach((marker) => {
+//       marker.setMap(null);
+//     });
+//     markers = [];
+
+//     // For each place, get the icon, name and location.
+//     const bounds = new google.maps.LatLngBounds();
+
+//     places.forEach((place) => {
+//       if (!place.geometry || !place.geometry.location) {
+//         console.log("Returned place contains no geometry");
+//         return;
+//       }
+
+//       const icon = {
+//         url: place.icon,
+//         size: new google.maps.Size(71, 71),
+//         origin: new google.maps.Point(0, 0),
+//         anchor: new google.maps.Point(17, 34),
+//         scaledSize: new google.maps.Size(25, 25),
+//       };
+
+//       // Create a marker for each place.
+//       markers.push(
+//         new google.maps.Marker({
+//           map,
+//           // icon,
+//           title: place.name,
+//           position: place.geometry.location,
+//         })
+//       );
+//       if (place.geometry.viewport) {
+//         // Only geocodes have viewport.
+//         bounds.union(place.geometry.viewport);
+//       } else {
+//         bounds.extend(place.geometry.location);
+//       }
+//       createMarker(place)
+//     });
+//     map.fitBounds(bounds);
+// });
   // Create the search box and link it to the UI element.
   const input = document.getElementById("pac-input");
   const searchBox = new google.maps.places.SearchBox(input);
@@ -179,7 +254,6 @@ function initMap() {
 
   let markers = [];
 
-  // [START maps_places_searchbox_getplaces]
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
   searchBox.addListener("places_changed", () => {
@@ -194,6 +268,10 @@ function initMap() {
       marker.setMap(null);
     });
     markers = [];
+    list.forEach((marker) => {
+      marker.setMap(null);
+    });
+    list = [];
 
     // For each place, get the icon, name and location.
     const bounds = new google.maps.LatLngBounds();
@@ -227,11 +305,13 @@ function initMap() {
       } else {
         bounds.extend(place.geometry.location);
       }
+      createMarker(place)
     });
-  map.fitBounds(bounds);
-});
+    map.fitBounds(bounds);
+  });
 // [END maps_places_searchbox_getplaces]
 }
+
 
 function getMyPlace() {
   var output = document.getElementById("result");
