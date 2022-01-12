45befa7dff12765bb53940bdcb1ea636af5c7448
kazuki0259 <ksw2070302@stu.o-hara.ac.jp>
2022-01-12 16:40:21 +0900

    現在地　表示

--- static/js/index.js
+++ static/js/index.js
@@ -433,3 +433,56 @@ directionsService.route(request, function(response, status) {
 //       position: latlng ,
 //   } ) ;
 // }
+
+
+window.onload = function(){
+  //1000ミリ秒（1秒）毎に関数「showNowDate()」を呼び出す
+  // setInterval("showNowDate()", 1000);
+  // setInterval("getMyPlace()", 1000);
+  // setInterval("c()", 1000);
+  
+  // getMyPlace();
+}
+
+var m;
+function c(){
+  m.setMap(null);
+}
+ 
+// //現在時刻を表示する関数
+// function showNowDate(){
+//   var dt = new Date();
+//   document.write(dt);
+// }
+
+
+function getMyPlace() {
+  console.log('sss');
+  var output = document.getElementById("result");
+  if (!navigator.geolocation){//Geolocation apiがサポートされていない場合
+    output.innerHTML = "<p>Geolocationはあなたのブラウザーでサポートされておりません</p>";
+    return;
+  }
+  function success(position) {
+    var latitude  = position.coords.latitude;//緯度
+    var longitude = position.coords.longitude;//経度
+    // output.innerHTML = '<p>緯度 ' + latitude + '° <br>経度 ' + longitude + '°</p>';
+    // 位置情報
+    var latlng = new google.maps.LatLng( latitude , longitude ) ;
+    // Google Mapsに書き出し
+    // var map = new google.maps.Map( document.getElementById( 'map' ) , {
+    //     zoom: 15 ,// ズーム値
+    //     center: latlng ,// 中心座標
+    // } ) ;
+    // マーカーの新規出力
+     m = new google.maps.Marker( {
+        map: map ,
+        position: latlng ,
+    } ) ;
+  };
+  function error() {
+    //エラーの場合
+    output.innerHTML = "座標位置を取得できません";
+  };
+  navigator.geolocation.getCurrentPosition(success, error);//成功と失敗を判断
+}
