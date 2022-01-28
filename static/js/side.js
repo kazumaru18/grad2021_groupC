50f64249461ee6384f0bacab1b47eb474d6d0640
kazumaru18 <k.tobe015@gmail.com>
2022-01-28 11:59:26 +0900

    0128 school

--- static/js/side.js
+++ static/js/side.js
@@ -1,46 +1,46 @@
 $(function(){
-  var duration = 300;
+  var duration = 250;
   var aside = $('.page_main > aside');
   var asidButton = aside.find('button')
       .on('click', function(){
           aside.toggleClass('open');
           if(aside.hasClass('open')){
-              aside.stop(true).animate({left: '-70px'}, duration, 'easeOutBack');
+              aside.stop(true).animate({left: '0px'}, duration);
               asidButton.find('img').attr('{% static "images/icon.png" %}');
           }else{
-              aside.stop(true).animate({left: '-350px'}, duration, 'easeInBack');
+              aside.stop(true).animate({left: '-350px'}, duration);
               asidButton.find('img').attr('{% static "images/icon.png" %}');
           };
       });
 });
 
 $(function(){
-  var duration = 300;
+  var duration = 250;
   var aside = $('.page_main2 > aside');
   var asidButton = aside.find('button')
       .on('click', function(){
           aside.toggleClass('open');
           if(aside.hasClass('open')){
-              aside.stop(true).animate({left: '-70px'}, duration, 'easeOutBack');
+              aside.stop(true).animate({left: '0px'}, duration);
               asidButton.find('img').attr('{% static "images/icon.png" %}');
           }else{
-              aside.stop(true).animate({left: '-350px'}, duration, 'easeInBack');
+              aside.stop(true).animate({left: '-350px'}, duration);
               asidButton.find('img').attr('{% static "images/icon.png" %}');
           };
       });
 });
 
 $(function(){
-  var duration = 300;
+  var duration = 250;
   var aside = $('.page_main3 > aside');
   var asidButton = aside.find('button')
       .on('click', function(){
           aside.toggleClass('open');
           if(aside.hasClass('open')){
-              aside.stop(true).animate({left: '-70px'}, duration, 'easeOutBack');
+              aside.stop(true).animate({left: '0px'}, duration);
               asidButton.find('img').attr('{% static "images/icon.png" %}');
           }else{
-              aside.stop(true).animate({left: '-350px'}, duration, 'easeInBack');
+              aside.stop(true).animate({left: '-350px'}, duration);
               asidButton.find('img').attr('{% static "images/icon.png" %}');
           };
       });
