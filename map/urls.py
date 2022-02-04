9db63c5b91fd21b06d3a4cef6a217a98f336ec53
kazumaru18 <k.tobe015@gmail.com>
2022-02-04 14:56:22 +0900

    0204 school

--- map/urls.py
+++ map/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path('map/', views.MapView.as_view(), name='map'),
   path('calendar/', views.CalendarView.as_view(), name='calendar'),
   path('describe/', views.DescriveView.as_view(), name='describe'),
+  path('onsei/', views.OnseiView.as_view(), name='onsei'),
 ]
